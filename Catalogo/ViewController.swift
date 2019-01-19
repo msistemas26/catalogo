f949c7f29683369341562e7f41c68bf4e8bf50ed
Raul Mantilla <raulmantilla@macbook-air-de-raul.local>
2019-01-19 09:55:36 -0500

    Initial Commit

---
+++ Catalogo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Catalogo
+//
+//  Created by Raul Mantilla on 19/01/19.
+//  Copyright © 2019 Raul Mantilla Assia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
